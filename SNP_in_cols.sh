6b62cef7dac769f8225d48a198ae562e8b2a2496
Guillermo Martinez Boggio <102411099+GMBog@users.noreply.github.com>
2024-04-24 06:50:34 -0500

    Create SNP_in_cols.sh
    From table with ID in the first col, and SNPs in the second col without spaces, move to a table with ID in the first col and one SNP per col

---
+++ SNP_in_cols.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=SNPdata
+#SBATCH --mail-user=@gmail.com
+#SBATCH --mail-type=END,FAIL
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50gb                    #memory per each job
+#SBATCH --time=90:00:00                                 #time per each job
+
+#Open the SNP file
+awk '{print $1}' ~/dirfiles/GenotypesFile > Ids
+awk '{print $2}' ~/dirfiles/GenotypeFile | sed 's/./& /g' > SNP_in_columns.txt
+
+#Paste ids to the SNP file
+paste -d ' ' Ids SNP_in_columns.txt > SNPdata.txt
+
+rm SNP_in_columns.txt
+rm Ids
+
+module load R
+Rscript Rcode_QCsnp.R
